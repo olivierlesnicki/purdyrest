52cb28c7b87cdb4aa7b7252177e53a317eb66698
Olivier Lesnicki <olivier.lesnicki@gmail.com>
2017-05-28 10:31:23 +0100

    Did a bunch of cleaning up

--- packages/micro-purdyrest-db/index.js
+++ packages/micro-purdyrest-db/index.js
@@ -1,3 +1,4 @@
+const { json } = require('micro');
 const micropurdyrest = require('micro-purdyrest');
 const shortid = require('shortid');
 const { ObjectID, MongoClient } = require('mongodb');
@@ -7,7 +8,6 @@ module.exports = (MONGODB_URI, options = {}) => {
   options.path = (options.path || '') + '/:collectionName';
 
   const db = fn => async (req, res) => {
-    const end = res.end;
     const db = await new Promise((resolve, reject) => {
       MongoClient.connect(MONGODB_URI, (err, db) => {
         if (err) { reject(err); }
@@ -30,14 +30,11 @@ module.exports = (MONGODB_URI, options = {}) => {
     if (Array.isArray(record)) {
       return record.map(serialize);
     } else {
-      return Object.assign({}, record.data, { _id: record._id });
+      return Object.assign({}, record, { _id: record._id });
     }
   };
 
-  const create = async (req, res) => {
-
-    console.log('here');
-
+  const create = db(async (req, res) => {
     const { db } = req;
     const { collectionName } = req.params;
     const data = await json(req);
@@ -47,12 +44,9 @@ module.exports = (MONGODB_URI, options = {}) => {
     const result = await collection.insert(data);
 
     return Object.assign({}, data, { _id });
-  };
+  });
 
-  const find = async (req, res) => {
-
-    console.log('here');
-
+  const find = db(async (req, res) => {
     const { db } = req;
     const { collectionName, id } = req.params;
     const collection = db.collection(collectionName);
@@ -66,14 +60,12 @@ module.exports = (MONGODB_URI, options = {}) => {
     }
 
     return serialize(record);
-  };
+  });
 
-  const filter = async (req, res) => {
-
-    console.log('here');
+  const filter = db(async (req, res) => {
 
     const { db } = req;
-    const { collectionName, id } = req.params;
+    const { collectionName } = req.params;
     const collection = db.collection(collectionName);
     let { filter } = req.query;
 
@@ -82,13 +74,11 @@ module.exports = (MONGODB_URI, options = {}) => {
     const records = await collection.find(filter).toArray();
 
     return serialize(records);
-  };
+  });
 
-  return db(
-    micropurdyrest({
-      create,
-      find,
-      filter
-    }, options)
-  );
+  return micropurdyrest({
+    create,
+    find,
+    filter
+  }, options)
 };
--- packages/micro-purdyrest-db/test/test.js
+++ packages/micro-purdyrest-db/test/test.js
@@ -14,25 +14,53 @@ const mongodb = {
 
 mock('mongodb', mongodb);
 
+const records = [{
+  _id: 'record_id',
+  foo: 'bar'
+}]
+
+const insert = (record) => Promise.resolve(record);
+const find = () => ({ toArray: () => Promise.resolve(records) });
+const findOne = () => Promise.resolve(records[0]);
+
+const collection = () => ({
+  insert,
+  find,
+  findOne
+});
+
+MongoClient.connect = (uri, callback) => {
+  callback(null, {
+    collection,
+    close: () => {}
+  });
+}
+
 const db = require('..');
 
 test('create passes', async t => {
-
-  const insert = sinon.stub();
-  const collection = sinon.stub().returns({
-    insert: sinon.stub().returns({})
-  });
-
-  MongoClient.connect = (uri, callback) => {
-    t.is(uri, 'mongodb-uri', 'does not use the correct mongodb uri');
-    callback(null, {
-      collection
-    });
-  }
-
   const service = micro(db('mongodb-uri'));
   const url = await listen(service);
-  const body = await request(url + '/models', { method: 'POST', json: true, body: { hello: 'world'}});
+  const body = await request(url + '/models', { method: 'POST', json: true, body: { foo: 'bar' } });
 
-  t.deepEqual(JSON.parse(body).test, 'woot')
+  t.truthy(body._id, 'does not return an _id');
+  t.is(body.foo, 'bar', 'does not return the fields');
+});
+
+test('find passes', async t => {
+  const service = micro(db('mongodb-uri'));
+  const url = await listen(service);
+  const body = await request(url + '/models/record_id', { method: 'GET', json: true });
+
+  t.truthy(body._id, 'does not return an _id');
+  t.is(body.foo, 'bar', 'does not return the fields');
+});
+
+test('filter passes', async t => {
+  const service = micro(db('mongodb-uri'));
+  const url = await listen(service);
+  const body = await request(url + '/models', { method: 'GET', json: true });
+
+  t.truthy(body[0]._id, 'does not return an _id');
+  t.is(body[0].foo, 'bar', 'does not return the fields');
 });
